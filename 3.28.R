0de3d7dd35dca69aa1756205485a81202fafbdce
hezy29 <67768189+hezy29@users.noreply.github.com>
2021-12-23 00:29:54 +0800

    Added codes for Q4, but still in progress; have a convergence problem regarding the weights

--- 3.28.R
+++ 3.28.R
@@ -37,7 +37,8 @@ X_SIS <- rnorm(n = N,
                mean = y[1],
                sd = sqrt(sigma2_n))
 X_t <- X_SIS
-W_SIS <- rbind(W_1 = rep(1, N))
+W_t <- rep(1, N)
+W_SIS <- rbind(W_1 = W_t)
 t <- 2
 while (t <= n) {
   X_t <- phi_1 * X_t + rnorm(n = N,
@@ -49,8 +50,55 @@ while (t <= n) {
       mean = A * cos(f * t + X_t),
       sd = sqrt(sigma2_eps)
     )
-  W_t <- W_SIS[t - 1,] * U_t
+  W_t <- W_t * U_t
   X_SIS <- rbind(X_SIS, X_t)
   W_SIS <- rbind(W_SIS, W_t)
+  t <- t + 1
+}
+
+## ---- Q4 ----
+X_SIS_RR <- rnorm(n = N,
+                  mean = y[1],
+                  sd = sqrt(sigma2_n))
+X_t <- X_SIS_RR
+W_t <- rep(1, N)
+W_SIS_RR <- W_t[1]
+t <- 2
+while (t <= n) {
+  X_t <- phi_1 * X_t + rnorm(n = N,
+                             mean = 0,
+                             sd = sqrt(sigma2_n))
+  U_t <-
+    pnorm(
+      q = y[t],
+      mean = A * cos(f * t + X_t),
+      sd = sqrt(sigma2_eps)
+    )
+  W_t <- W_t * U_t
+  if (max(W_t) == 0) {
+    print(t)
+    break
+  }
+  W_t.bar <- mean(W_t)
+  k_t <- floor(W_t / W_t.bar)
+  N_r_t <- N - sum(k_t)
+  index.all_t <- c()
+  for (k in 1:max(k_t)) {
+    index.all_t <- c(index.all_t, which(k_t >= k))
+    k <- k + 1
+  }
+  if (N_r_t > 0) {
+    p.resample_t <- (W_t / W_t.bar - k_t) / N_r_t
+    index.resample_t <-
+      sample(
+        x = 1:N,
+        size = N_r_t,
+        replace = TRUE,
+        prob = p.resample_t
+      )
+    index.all_t <- c(index.all_t, index.resample_t)
+  }
+  X_SIS_RR <- rbind(X_SIS_RR, X_t)[, index.all_t]
+  W_SIS_RR <- c(W_SIS_RR, W_t.bar)
   t <- t + 1
 }
